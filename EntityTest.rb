b394d6a2ed0b361d316090dd5ef13c3d3bad2ff6
Simon Richards <srichards@slingshot.co.nz>
2010-11-13 13:22:52 +1300

    sdfsafd

--- EntityTest.rb
+++ EntityTest.rb
@@ -1,4 +1,6 @@
+require 'rubygems'
 require 'test/unit'
+require 'entity'
 class EntityTest < Test::Unit::TestCase
     def setup
     end
@@ -7,7 +9,7 @@ class EntityTest < Test::Unit::TestCase
     end
 
     def test_death
-        testEntity = Entity.new
+        testEntity = Entity::new
         testEntity.hit(100)
         assert(testEntity.state == State::DEAD, 'Kill the tank and check it dies')
     end
